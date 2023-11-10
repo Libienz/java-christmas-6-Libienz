c35bf66fccd199bdc22e00a96a73131a0e7056c2
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-11 07:44:15 +0900

    feat: OrderDate 도메인 속성 정의 및 할당 검증 구현

---
+++ src/main/java/christmas/domain/OrderDate.java
@@ -0,0 +1,34 @@
+package christmas.domain;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
+
+public class OrderDate {
+    private static final Integer YEAR = 2023;
+    private static final Integer MONTH = 12;
+    private static final Integer START_DAY_OF_DECEMBER = 1;
+    private static final Integer END_DAY_OF_DECEMBER = 31;
+    private static final String INVALID_RANGE_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+
+    private final Integer dayOfMonth;
+
+    public OrderDate(Integer dayOfMonth) {
+        validate(dayOfMonth);
+        this.dayOfMonth = dayOfMonth;
+    }
+
+    public static OrderDate from(int dayOfMonth) {
+        return new OrderDate(dayOfMonth);
+    }
+
+    private void validate(Integer dayOfMonth) {
+        validateRange(dayOfMonth);
+    }
+
+    private void validateRange(Integer dayOfMonth) {
+        if (dayOfMonth < START_DAY_OF_DECEMBER || dayOfMonth > END_DAY_OF_DECEMBER) {
+            throw new IllegalArgumentException(INVALID_RANGE_MESSAGE);
+        }
+    }
+}
