46c2e63a4f9a44b3ed32b6e9395a846b35df7ee6
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-12 05:48:15 +0900

    refactor: EnumMap으로 관리하던 orders를 OrderItem의 리스트로 관리하도록 개선

--- src/main/java/christmas/domain/OrderItems.java
+++ src/main/java/christmas/domain/OrderItems.java
@@ -1,36 +1,15 @@
 package christmas.domain;
 
-import java.util.EnumMap;
+import java.util.List;
 
 public class OrderItems {
-    private static final Integer MAX_ORDER_COUNT = 20;
-    private static final Integer MIN_ORDER_COUNT = 1;
-    private static final String INVALID_RANGE_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    private List<OrderItem> orderItems;
 
-    private final EnumMap<MenuItem, Integer> orderItems;
-
-    private OrderItems(EnumMap<MenuItem, Integer> orderItems) {
-        validate(orderItems);
+    private OrderItems(List<OrderItem> orderItems) {
         this.orderItems = orderItems;
     }
 
-    public static OrderItems from(EnumMap<MenuItem, Integer> orderItems) {
+    public static OrderItems from(List<OrderItem> orderItems) {
         return new OrderItems(orderItems);
     }
-
-    private void validate(EnumMap<MenuItem, Integer> order) {
-        validateOrderCount(order);
-    }
-
-    private void validateOrderCount(EnumMap<MenuItem, Integer> order) {
-        if (isInvalidOrderCount(order)) {
-            throw new IllegalArgumentException(INVALID_RANGE_MESSAGE);
-        }
-    }
-
-    private boolean isInvalidOrderCount(EnumMap<MenuItem, Integer> order) {
-        return order.values().stream()
-                .anyMatch(count -> count < MIN_ORDER_COUNT || count > MAX_ORDER_COUNT);
-    }
-
 }
