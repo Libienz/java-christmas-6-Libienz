e33d84e352388319d5e507969ea39b8393e36b61
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-12 08:58:17 +0900

    feat: Order에 OrderDate가 기간에 속하는지 확인하는 메서드 추가

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,15 +1,19 @@
 package christmas.domain;
 
 public class Order {
-    private final OrderItems order;
+    private final OrderItems orderItems;
     private final OrderDate orderDate;
 
-    private Order(OrderItems order, OrderDate orderDate) {
-        this.order = order;
+    private Order(OrderItems orderItems, OrderDate orderDate) {
+        this.orderItems = orderItems;
         this.orderDate = orderDate;
     }
 
     public static Order of(OrderItems order, OrderDate orderDate) {
         return new Order(order, orderDate);
+    }
+
+    public boolean isOrderDateInPeriod(int startDay, int endDay) {
+        return orderDate.isInPeriod(startDay, endDay);
     }
 }
