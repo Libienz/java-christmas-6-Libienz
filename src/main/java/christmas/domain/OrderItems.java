d9ff178867c98be9d0f770a5f26a79b4f0e33efb
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-12 08:55:53 +0900

    feat: OrderItems 중복 검증 구현

--- src/main/java/christmas/domain/OrderItems.java
+++ src/main/java/christmas/domain/OrderItems.java
@@ -3,13 +3,32 @@ package christmas.domain;
 import java.util.List;
 
 public class OrderItems {
+    private static final String INVALID_ORDER = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
     private List<OrderItem> orderItems;
 
     private OrderItems(List<OrderItem> orderItems) {
+        validate(orderItems);
         this.orderItems = orderItems;
     }
 
     public static OrderItems from(List<OrderItem> orderItems) {
         return new OrderItems(orderItems);
+    }
+
+    private void validate(List<OrderItem> orderItems) {
+        validateDuplicateOrderItem(orderItems);
+    }
+
+    private void validateDuplicateOrderItem(List<OrderItem> orderItems) {
+        if (hasDuplicateItem(orderItems)) {
+            throw new IllegalArgumentException(INVALID_ORDER);
+        }
+    }
+
+    private boolean hasDuplicateItem(List<OrderItem> orderItems) {
+        return orderItems.size() != orderItems.stream()
+                .map(OrderItem::getMenuName)
+                .distinct()
+                .count();
     }
 }
