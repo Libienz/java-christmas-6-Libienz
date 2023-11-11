137506eb9ee3b1136bc0e4f3a473c69bee827c9c
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-11 10:05:24 +0900

    refactor: Order 클래스의 주문 내역 속성을 EnumMap을 감싸는 OrderItems로 변경

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,37 +1,15 @@
 package christmas.domain;
 
-import java.util.EnumMap;
-
 public class Order {
-    private static final Integer MAX_ORDER_COUNT = 20;
-    private static final Integer MIN_ORDER_COUNT = 1;
-    private static final String INVALID_RANGE_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
-
-    private final EnumMap<MenuItem, Integer> order;
+    private final OrderItems order;
     private final OrderDate orderDate;
 
-    private Order(EnumMap<MenuItem, Integer> order, OrderDate orderDate) {
-        validate(order, orderDate);
+    private Order(OrderItems order, OrderDate orderDate) {
         this.order = order;
         this.orderDate = orderDate;
     }
 
-    public static Order of(EnumMap<MenuItem, Integer> order, OrderDate orderDate) {
+    public static Order of(OrderItems order, OrderDate orderDate) {
         return new Order(order, orderDate);
-    }
-
-    private void validate(EnumMap<MenuItem, Integer> order, OrderDate orderDate) {
-        validateOrderCount(order);
-    }
-
-    private void validateOrderCount(EnumMap<MenuItem, Integer> order) {
-        if (isInvalidOrderCount(order)) {
-            throw new IllegalArgumentException(INVALID_RANGE_MESSAGE);
-        }
-    }
-
-    private boolean isInvalidOrderCount(EnumMap<MenuItem, Integer> order) {
-        return order.values().stream()
-                .anyMatch(count -> count < MIN_ORDER_COUNT || count > MAX_ORDER_COUNT);
     }
 }
--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -1,36 +1,8 @@
 package christmas.domain;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import java.util.EnumMap;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("주문 내역 테스트")
 class OrderTest {
-    @DisplayName("주문 개수의 범위를 벗어나는 입력은 생성 시 검증에 실패한다")
-    @ParameterizedTest()
-    @ValueSource(ints = {0, 21, 22})
-    void testInvalidRangeOrderCountOrder(int orderCount) {
-        EnumMap<MenuItem, Integer> order = new EnumMap<>(MenuItem.class);
-        order.put(MenuItem.BARBECUE_RIBS, orderCount);
-        OrderDate orderDate = OrderDate.from(1);
 
-        assertThatThrownBy(() -> Order.of(order, orderDate))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-    }
-
-    @DisplayName("올바른 입력은 생성 시 검증을 통과한다")
-    @ParameterizedTest()
-    @ValueSource(ints = {1, 2, 20})
-    void testValidRangeOrderCountOrder(int orderCount) {
-        EnumMap<MenuItem, Integer> order = new EnumMap<>(MenuItem.class);
-        order.put(MenuItem.BARBECUE_RIBS, orderCount);
-        OrderDate orderDate = OrderDate.from(1);
-
-        assertThatNoException().isThrownBy(() -> Order.of(order, orderDate));
-    }
 }
