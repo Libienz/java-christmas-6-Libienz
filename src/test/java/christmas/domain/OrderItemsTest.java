02825cbfca8bfb5c5534c29302b2d8638f011d1b
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-11 10:03:32 +0900

    test: OrderItems 검증 테스트 작성

---
+++ src/test/java/christmas/domain/OrderItemsTest.java
@@ -0,0 +1,34 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.EnumMap;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("주문 날짜 테스트")
+class OrderItemsTest {
+    @DisplayName("주문 개수의 범위를 벗어나는 입력은 생성 시 검증에 실패한다")
+    @ParameterizedTest()
+    @ValueSource(ints = {0, 21, 22})
+    void testInvalidRangeOrderCountOrder(int orderCount) {
+        EnumMap<MenuItem, Integer> order = new EnumMap<>(MenuItem.class);
+        order.put(MenuItem.BARBECUE_RIBS, orderCount);
+
+        assertThatThrownBy(() -> OrderItems.from(order))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+
+    @DisplayName("올바른 입력은 생성 시 검증을 통과한다")
+    @ParameterizedTest()
+    @ValueSource(ints = {1, 2, 20})
+    void testValidRangeOrderCountOrder(int orderCount) {
+        EnumMap<MenuItem, Integer> order = new EnumMap<>(MenuItem.class);
+        order.put(MenuItem.BARBECUE_RIBS, orderCount);
+
+        assertThatNoException().isThrownBy(() -> OrderItems.from(order));
+    }
+}
