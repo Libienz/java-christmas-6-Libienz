f7922ddd2d1202fda604a5746f6a4596b6a710f9
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-12 09:07:18 +0900

    feat: DiscountResultDto 속성 정의 및 구현

--- src/main/java/christmas/dto/DiscountResultDto.java
+++ src/main/java/christmas/dto/DiscountResultDto.java
@@ -1,4 +1,23 @@
 package christmas.dto;
 
 public class DiscountResultDto {
+    private String discountDescription;
+    private Integer discountAmount;
+
+    private DiscountResultDto(String discountDescription, Integer discountAmount) {
+        this.discountDescription = discountDescription;
+        this.discountAmount = discountAmount;
+    }
+
+    public static DiscountResultDto of(String discountDescription, Integer discountAmount) {
+        return new DiscountResultDto(discountDescription, discountAmount);
+    }
+
+    public String getDiscountDescription() {
+        return discountDescription;
+    }
+
+    public Integer getDiscountAmount() {
+        return discountAmount;
+    }
 }
