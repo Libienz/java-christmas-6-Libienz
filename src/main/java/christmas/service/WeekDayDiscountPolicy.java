ce1e20058761b5c106ed2fabdc775ddfc959117a
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-12 14:18:52 +0900

    refactor: 사용되지 않는 상수 삭제 개선

--- src/main/java/christmas/service/WeekDayDiscountPolicy.java
+++ src/main/java/christmas/service/WeekDayDiscountPolicy.java
@@ -5,8 +5,6 @@ import christmas.domain.Order;
 import christmas.dto.DiscountResultDto;
 
 public class WeekDayDiscountPolicy implements DiscountPolicy {
-    private static final Integer EVENT_START_DAY = 1;
-    private static final Integer EVENT_END_DAY = 31;
     private static final MenuCategory DISCOUNT_MENU_CATEGORY = MenuCategory.DESSERT;
     private static final String DISCOUNT_DESCRIPTION = "평일 할인";
     private static final Integer DISCOUNT_AMOUNT_PER_ITEM = 2023;
