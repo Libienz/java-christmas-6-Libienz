93cea1a97081714dc5839e62097630739efeca19
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-13 16:00:26 +0900

    test: 샴페인 증정 정책 테스트 작성

---
+++ src/test/java/christmas/service/ChampagneGiveawayPolicyTest.java
@@ -0,0 +1,74 @@
+package christmas.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.domain.MenuItem;
+import christmas.domain.Order;
+import christmas.domain.OrderDate;
+import christmas.domain.OrderItem;
+import christmas.domain.OrderItems;
+import christmas.dto.GiveawayResultDto;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("샴페인 증정 테스트 ")
+class ChampagneGiveawayPolicyTest {
+    private ChampagneGiveawayPolicy champagneGiveawayPolicy;
+
+    @BeforeEach
+    void setUp() {
+        champagneGiveawayPolicy = new ChampagneGiveawayPolicy();
+    }
+
+    @DisplayName("총 주문 금액이 120000원 이상이면 샴페인을 증정할 수 있다")
+    @Test
+    void testChampagneGiveawayAchievable() {
+        OrderItem item1 = OrderItem.of(MenuItem.BARBECUE_RIBS, 3);
+        OrderItem item2 = OrderItem.of(MenuItem.CAESAR_SALAD, 3);
+        OrderItem item3 = OrderItem.of(MenuItem.ZERO_COLA, 3);
+        List<OrderItem> orderItems = List.of(item1, item2, item3);
+
+        Order order = Order.of(OrderItems.from(orderItems), OrderDate.from(5));
+        assertThat(champagneGiveawayPolicy.supports(order)).isTrue();
+    }
+
+    @DisplayName("총 주문 금액이 120000원 미만이면 샴페인을 증정 받지 못한다")
+    @Test
+    void testChampagneGiveawayNonAchievable() {
+        OrderItem item1 = OrderItem.of(MenuItem.ZERO_COLA, 3);
+        List<OrderItem> orderItems = List.of(item1);
+
+        Order order = Order.of(OrderItems.from(orderItems), OrderDate.from(5));
+        assertThat(champagneGiveawayPolicy.supports(order)).isFalse();
+    }
+
+    @DisplayName("증정하는 물품은 샴페인이다")
+    @Test
+    void testGiveawayMenu() {
+        OrderItem item1 = OrderItem.of(MenuItem.BARBECUE_RIBS, 3);
+        OrderItem item2 = OrderItem.of(MenuItem.CAESAR_SALAD, 3);
+        OrderItem item3 = OrderItem.of(MenuItem.ZERO_COLA, 3);
+        List<OrderItem> orderItems = List.of(item1, item2, item3);
+
+        Order order = Order.of(OrderItems.from(orderItems), OrderDate.from(5));
+        GiveawayResultDto giveawayResultDto = champagneGiveawayPolicy.applyGiveaway(order);
+
+        assertThat(giveawayResultDto.getGiveaway()).isEqualTo(MenuItem.CHAMPAGNE);
+    }
+
+    @DisplayName("증정되는 개수는 1개이다")
+    @Test
+    void testGiveawayMenuCount() {
+        OrderItem item1 = OrderItem.of(MenuItem.BARBECUE_RIBS, 3);
+        OrderItem item2 = OrderItem.of(MenuItem.CAESAR_SALAD, 3);
+        OrderItem item3 = OrderItem.of(MenuItem.ZERO_COLA, 3);
+        List<OrderItem> orderItems = List.of(item1, item2, item3);
+
+        Order order = Order.of(OrderItems.from(orderItems), OrderDate.from(5));
+        GiveawayResultDto giveawayResultDto = champagneGiveawayPolicy.applyGiveaway(order);
+
+        assertThat(giveawayResultDto.getCount()).isEqualTo(1);
+    }
+}
