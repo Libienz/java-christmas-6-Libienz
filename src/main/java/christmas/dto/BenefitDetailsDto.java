a668e89a4a928342765712ed541706715288f7d3
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-14 00:14:12 +0900

    feat: BenefitDetailsDto 정적 팩토리 메서드 of 추가

--- src/main/java/christmas/dto/BenefitDetailsDto.java
+++ src/main/java/christmas/dto/BenefitDetailsDto.java
@@ -1,6 +1,7 @@
 package christmas.dto;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public class BenefitDetailsDto {
     private final List<BenefitDetailDto> benefitDetailDtos;
@@ -13,7 +14,25 @@ public class BenefitDetailsDto {
         return new BenefitDetailsDto(benefitDetailDtos);
     }
 
+    public static BenefitDetailsDto of(DiscountResultsDto discountResultsDto, GiveawayResultsDto giveawayResultsDto) {
+        List<BenefitDetailDto> combinedBenefits = combineToBenefitDetails(discountResultsDto, giveawayResultsDto);
+        return BenefitDetailsDto.from(combinedBenefits);
+    }
+
+    private static List<BenefitDetailDto> combineToBenefitDetails(DiscountResultsDto discountResultsDto,
+                                                                  GiveawayResultsDto giveawayResultsDto) {
+        List<BenefitDetailDto> combinedBenefits = Stream.concat(
+                discountResultsDto.getDiscountResultDtos().stream()
+                        .map(result -> BenefitDetailDto.of(result.getDiscountDescription(),
+                                result.getDiscountAmount())),
+                giveawayResultsDto.getGiveawayResultDtos().stream()
+                        .map(result -> BenefitDetailDto.of(result.getDescription(), result.getDiscountAmount()))
+        ).toList();
+        return combinedBenefits;
+    }
+
     public List<BenefitDetailDto> getBenefitDetailDtos() {
         return benefitDetailDtos;
     }
+
 }
