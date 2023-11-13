c0635f69ea8801bc7ad5c7ae4c94b882ea24c457
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-13 16:00:07 +0900

    feat: ChampagneGiveawayPolicy 샴페인 증정 정책 구현

---
+++ src/main/java/christmas/service/ChampagneGiveawayPolicy.java
@@ -0,0 +1,21 @@
+package christmas.service;
+
+import christmas.domain.MenuItem;
+import christmas.domain.Order;
+import christmas.dto.GiveawayResultDto;
+
+public class ChampagneGiveawayPolicy implements GiveawayPolicy {
+    private static final MenuItem GIVEAWAY_MENU_ITEM = MenuItem.CHAMPAGNE;
+    private static final Integer THRESHOLD_AMOUNT = 120000;
+    private static final Integer GIVEAWAY_COUNT = 1;
+
+    @Override
+    public Boolean supports(Order order) {
+        return order.calculatePrice() >= THRESHOLD_AMOUNT;
+    }
+
+    @Override
+    public GiveawayResultDto applyGiveaway(Order order) {
+        return GiveawayResultDto.of(GIVEAWAY_MENU_ITEM, GIVEAWAY_COUNT);
+    }
+}
