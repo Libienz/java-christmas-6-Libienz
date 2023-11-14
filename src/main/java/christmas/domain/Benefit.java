cf900532ee7f10bdf01562dec4638a4b606fa65c
Kuen Hee Lee <Leekh10195@gmail.com>
2023-11-14 21:40:21 +0900

    feat: Benefit 도메인 속성 정의 및 생성 로직 작성

---
+++ src/main/java/christmas/domain/Benefit.java
@@ -0,0 +1,15 @@
+package christmas.domain;
+
+public class Benefit {
+    private final DiscountDetails discountDetails;
+    private final FreeGifts freeGifts;
+
+    private Benefit(DiscountDetails discountDetails, FreeGifts freeGifts) {
+        this.discountDetails = discountDetails;
+        this.freeGifts = freeGifts;
+    }
+
+    public static Benefit of(DiscountDetails discountDetails, FreeGifts freeGifts) {
+        return new Benefit(discountDetails, freeGifts);
+    }
+}
